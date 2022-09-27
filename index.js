ffb732945b9c9500ac571c31cfbebe50216b8638
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-27 08:43:13 +0200

    avances

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+//http://www.omdbapi.com/?apikey=[yourkey]&
+const key = "3c4869fc";
 console.log('Hi there!');
+
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/',{
+        params : {
+            apikey: '3c4869fc',
+            s: 'titanic'
+        }
+    });
+    console.log(response.data);
+};
+
+fetchData();
+

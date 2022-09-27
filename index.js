2545c0b310becfeb374b6da8c2c6498af5ed4f66
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-27 14:56:38 +0200

    avances

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
-const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/',{
-        params : {
-            apikey: '3c4869fc',
-            s: searchTerm
+const fetchData = async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+        apikey: 'd9835cc5',
+        s: searchTerm
         }
     });
-    if (response.data.Error)
-        return []       
-    else
-        return response.data.Search;
-};
 
+    if (response.data.Error) {
+        return [];
+    }
+
+    return response.data.Search;
+};
+  
 const root = document.querySelector('.autocomplete');
 root.innerHTML = `
-<label><b>Search for a movie</b></label>
-<input class="input"/>
-<div class="dropdown">
-    <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-    </div>
-</div>
+   <label><b>Search For a Movie</b></label>
+   <input class="input" />
+   <div class="dropdown">
+      <div class="dropdown-menu">
+         <div class="dropdown-content results"></div>
+      </div>
+   </div>
 `;
 
 const input = document.querySelector('input');
 const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
 
-const onInput = async (event) => {    
-    const movies = await fetchData(event.target.value);
+const onInput = async event => {   
+   const movies = await fetchData(event.target.value);
 
-    dropdown.classList.add('is-active');
-    for(let movie of movies) {
-        const option  = document.createElement('a');
-        option.classList.add('dropdown-item');
-        option.innerHTML = `
-            <img src="${movie.Poster}"/>
-            ${movie.Title}
-        `;
-        resultsWrapper.appendChild(option);
-    }
+   if(!movies.length > 0){
+      dropdown.classList.remove('is-active');
+      return;
+   }
+
+   resultsWrapper.innerHTML = '';
+   dropdown.classList.add('is-active');
+
+   for (let movie of movies) {
+      const option = document.createElement('a');
+      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+      option.classList.add('dropdown-item');
+      option.innerHTML = `
+         <img src="${imgSrc}" />
+         ${movie.Title}
+      `;
+
+      resultsWrapper.appendChild(option);
+   }
 };
+input.addEventListener('input', debounce(onInput, 500));
 
-input.addEventListener('input', debounce(onInput,1000));
+document.addEventListener('click', event =>{
+   if(!root.contains(event.target)){
+      dropdown.classList.remove('is-active');
+   }
+   console.log(event.target);
+})

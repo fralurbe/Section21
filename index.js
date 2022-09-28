e99974608c098b84a02f90c9e0077265dbfcd742
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-28 14:06:49 +0200

    avances

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-const fetchData = async searchTerm => {
-   const response = await axios.get('http://www.omdbapi.com/', {
-      params: {
-      apikey: 'd9835cc5',
-      s: searchTerm
-      }
-   });
-
-   if (response.data.Error) {
-      return [];
-   }
-
-   return response.data.Search;
-};
-
-createAutoComplete({
-   root : document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
    renderOption(movie){      
       const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
       return `
          <img src="${imgSrc}" />
          ${movie.Title} (${movie.Year})
       `;
-   }, 
+   },
    onOptionSelect(movie){
       onMovieSelect(movie);
    },
    inputValue(movie){
       return movie.Title;
+   },
+   async fetchData (searchTerm) {
+      const response = await axios.get('http://www.omdbapi.com/', {
+         params: {
+            apikey: 'd9835cc5',
+            s: searchTerm
+         }
+      });
+   
+      if (response.data.Error) {
+         return [];
+      }
+   
+      return response.data.Search;
    }
+}
+
+createAutoComplete({
+   ...autoCompleteConfig,
+   root : document.querySelector('#left-autocomplete'),   
+});
+
+createAutoComplete({
+   ...autoCompleteConfig,
+   root : document.querySelector('#right-autocomplete'),   
 });
 
 const onMovieSelect = async (movie) => {
